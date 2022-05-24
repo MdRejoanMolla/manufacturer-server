575afa03bf3b4fc2bf34097b1c56b444c4c835cf
MdRejoanMolla <mollamdrejoan@gmail.com>
2022-05-25 00:57:09 +0600

    second

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ require('dotenv').config();
 const port = process.env.PORT || 5000
 
 app.use(cors());
-app.use(express.json());
-const jwt = require('jsonwebtoken');
+app.use(express.json()); const jwt = require('jsonwebtoken');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fgjem.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
