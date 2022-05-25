3cecba4519481c1a62699712a0473f76691e2a44
MdRejoanMolla <mollamdrejoan@gmail.com>
2022-05-25 15:20:47 +0600

    four

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 const app = express()
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000
 
 app.use(cors());
-app.use(express.json()); const jwt = require('jsonwebtoken');
+app.use(express.json());
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fgjem.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+            return res.status(401).send({ message: 'UnAuthorized access' });
+      }
+      const token = authHeader.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+            if (err) {
+                  return res.status(403).send({ message: 'Forbidden access' })
+            }
+            req.decoded = decoded;
+            next();
+      });
+}
 
 async function run() {
       try {
@@ -23,6 +40,28 @@ async function run() {
                   const tools = await cursor.toArray();
                   res.send(tools);
             })
+
+            app.get('/user', verifyJWT, async (req, res) => {
+                  const users = await userCollection.find().toArray();
+                  res.send(users);
+            });
+
+            app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+                  const email = req.params.email;
+                  const requester = req.decoded.email;
+                  const filter = { email: email };
+
+                  const updateDoc = {
+                        $set: { role: 'admin' },
+                  };
+                  const result = await userCollection.updateOne(filter, updateDoc);
+                  res.send(result);
+
+                  res.send(result);
+
+
+            })
+
             app.get('/addReview', async (req, res) => {
                   const query = {};
                   const cursor = reviewCollection.find(query);
@@ -40,7 +79,7 @@ async function run() {
                   const tool = await toolCollection.findOne(query);
                   res.send(tool)
             })
-            app.put('/user/:id', async (req, res) => {
+            app.put('/user/:email', async (req, res) => {
                   const email = req.params.email;
                   const user = req.body;
                   const filter = { email: email };
@@ -49,7 +88,8 @@ async function run() {
                         $set: user,
                   };
                   const result = await userCollection.updateOne(filter, updateDoc, options);
-                  res.send(result)
+                  const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+                  res.send({ result, token });
             })
 
 
