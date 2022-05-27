149ab506843acc34eb9f75d5cb83f2d58c59f01f
MdRejoanMolla <mollamdrejoan@gmail.com>
2022-05-27 21:28:13 +0600

    end

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const app = express()
 const cors = require('cors');
 require('dotenv').config();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5500
 
 app.use(cors());
 app.use(express.json());
